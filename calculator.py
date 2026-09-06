8b3b031cd9ff09d510c5dfd29aeb80c6e320b1cb
tim <timkiberone8@gmail.com>
2026-09-06 20:10:29 +0300

    3

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# operation = input("Введите операцию (сложение, вычитание, умножение, деление): ").strip().lower()
+
+# if operation == "сложение":
+#     result = a + b
+# elif operation == "вычитание":
+#     result = a - b
+# elif operation == "умножение":
+#     result = a * b
+# elif operation == "деление":
+#     result = a / b
+# else:
+#     result = "Неизвестная операция"
+
+
+# print(result)
+
+fruits = ["яблоко", "банан", "вишня"]
+
+for fruit in fruits:
+    print(fruit)
